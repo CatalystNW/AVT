542e44ea989644ddbeaf602fede2f85a891d96f9
gougeogl <gougeogl@oregonstate.edu>
2020-05-02 14:15:03 -0700

    modified:   routes/projectsummary.js 	modified:   routes/report.js
    	First attempt: Implement query for completed projects
    	mostly back-end. Should only show in console.

--- routes/report.js
+++ routes/report.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var db = require('../mongoose/connection');
 
 var User = require('../models/userPackage');
+var ProjectWrapUpPackages = require('../models/projectWrapUpPackage'); // GLEN added this line
 
 var Promise = require('bluebird'); // Import promise engine
 mongoose.Promise = require('bluebird'); // Tell mongoose we are using the Bluebird promise library
@@ -13,11 +14,106 @@ var ObjectId = require('mongodb').ObjectID;
 
 module.exports = function(passport){
     router.get('/', isLoggedIn, function(req, res){
-        res.render('projectsumreport');
+            // stuff here.. ?
+    // some more stuff ??
+    console.log('report.js total projects aggregation starting');
+
+    // create object 'payload' to return
+    var payload = {};
+
+    Promise.props({
+        completedProjects: ProjectWrapUpPackages.aggregate([{
+            $match: {
+                "signup_sheet_office.complete": true
+            }
+            },{
+            $group: {
+                _id: {
+                    $year: { 
+                        date: "$signup_sheet_office.completed_on"
+                    }
+                },
+                "total_projects": {$push: { $year: { date: "$signup_sheet_office.completed_on" } } }
+            } 
+            }, {
+            $project: {
+                _id: 0,
+                year: {$arrayElemAt: ["$total_projects", 0] },
+                "# projects completed": {$size: "$total_projects"}
+            }
+        }]).execAsync()
     })
-    return router
+    .then(function(results) {          
+        if(!results){
+            console.log('report.js ERROR: total projects aggregation failure!');
+        }
+        else {
+            console.log('report.js total projects aggregation passed.');
+            res.locals.results = results;
+            next();
+        }
+    }) 
+    .catch(function(err){
+        console.err(err);
+    })
+    .catch(next);
+    
+    payload.completedProjects = res.locals.results.completedProjects;
+    res.render('projectsumreport', payload);
+    // trace
+    console.log('completed projects should be: ' + payload.completedProjects);
+    })
+
 };
 
+
+/***************************************************************************** */
+/* Glen's Section 1st Attempt */
+/*
+router.post('/', function(req, res, next) {
+    // stuff here.. ?
+    // some more stuff ??
+    console.log('report.js total projects aggregation starting');
+
+    Promise.props({
+        completedProjects: ProjectWrapUpPackages.aggregate([{
+            $match: {
+                "signup_sheet_office.complete": true
+            }
+            },{
+            $group: {
+                _id: {
+                    $year: { 
+                        date: "$signup_sheet_office.completed_on"
+                    }
+                },
+                "total_projects": {$push: { $year: { date: "$signup_sheet_office.completed_on" } } }
+            } 
+            }, {
+            $project: {
+                _id: 0,
+                year: {$arrayElemAt: ["$total_projects", 0] },
+                "# projects completed": {$size: "$total_projects"}
+            }
+        }]).execAsync()
+    })
+    .then(function(results) {          
+        if(!results){
+            console.log('report.js ERROR: total projects aggregation failure!');
+        }
+        else {
+            console.log('report.js total projects aggregation passed.');
+            res.locals.results = results;
+            next();
+        }
+    }) 
+    .catch(function(err){
+        console.err(err);
+    })
+    .catch(next);
+});
+*/
+/***************************************************************************** */
 function isLoggedIn(req, res, next) {
 
     if(req.isAuthenticated()) {
@@ -27,37 +123,82 @@ function isLoggedIn(req, res, next) {
             user: User.findOne({'_id' : ObjectId(userID)}).lean().execAsync()
         })
         .then(function (results) {
-                if (!results) {
-                    res.redirect('/user/logout');
+            if (!results) {
+                res.redirect('/user/logout');
+            }
+            else {
+                if(results.user.user_status == "ACTIVE") {
+                    res.locals.assign_tasks = results.user.assign_tasks;
+        
+                    if(results.user.user_role == "VET" || results.user.user_role == "ADMIN") {
+                        res.locals.email = results.user.contact_info.user_email;
+                        res.locals.role = results.user.user_role;
+                        res.locals.user_roles = results.user.user_roles;
+                        return next();
+
+                    }
+                    else if(results.user.user_roles !== undefined  && results.user.user_roles.indexOf('PROJECT_MANAGEMENT') > -1) {
+                        res.locals.email = results.user.contact_info.user_email;
+                        res.locals.role = results.user.user_role;
+                        res.locals.user_roles = results.user.user_roles;
+                        return next();
+
+                    }
+
+                    else {
+                        
+                        res.redirect('/user/logout');
+                    }
                 }
                 else {
-                    if(results.user.user_status == "ACTIVE") {
-          res.locals.assign_tasks = results.user.assign_tasks;
-          
-                        if(results.user.user_role == "VET" || results.user.user_role == "ADMIN") {
-                            res.locals.email = results.user.contact_info.user_email;
-                            res.locals.role = results.user.user_role;
-                            res.locals.user_roles = results.user.user_roles;
-                            return next();
+                    //user not active
+                    console.log("user not active");
+                    res.redirect('/user/logout');
+                }
+            }
 
-                        }
-                        else if(results.user.user_roles !== undefined  && results.user.user_roles.indexOf('PROJECT_MANAGEMENT') > -1) {
-                            res.locals.email = results.user.contact_info.user_email;
-                            res.locals.role = results.user.user_role;
-                            res.locals.user_roles = results.user.user_roles;
-                            return next();
+        })
+        .catch(function(err) {console.error(err);
+        })
+        .catch(next);
+    }
+    else {
+        console.log("no user id");
+        res.redirect('/user/login');
+    }
+}
 
-                        }
+//COPIED FROM: projectsummary.js (/route/)
+//post request authenticator.  Checks if user is an admin or vetting agent
+function isLoggedInPost(req, res, next) {
+    if(req.isAuthenticated()) {
+        var userID = req.user._id.toString();
+        var ObjectId = require('mongodb').ObjectID;
 
-                        else {
-                            
-                            res.redirect('/user/logout');
-                        }
+        Promise.props({
+            user: User.findOne({'_id' : ObjectId(userID)}).lean().execAsync()
+        })
+        .then(function (results) {
+                if (!results) {
+                    //user not found in db.  Route to error handler
+                    res.locals.status = 406;
+                    return next('route');
+                }
+                else {
+
+                    if(results.user.user_role == "VET" || results.user.user_role == "ADMIN") {
+                        return next();
+
+                    }
+                    else if (results.user.user_roles !== undefined && (results.user.user_roles.indexOf('VET') >-1|| results.user.user_roles.indexOf('PROJECT_MANAGEMENT') >-1))
+                    {
+                        
+                        return next();
                     }
                     else {
-                        //user not active
-                        console.log("user not active");
-                        res.redirect('/user/logout');
+                        //user is not a vetting agent or admin, route to error handler
+                        res.locals.status = 406;
+                        return next('route');
                     }
                 }
 
@@ -69,7 +210,9 @@ function isLoggedIn(req, res, next) {
      .catch(next);
     }
     else {
+        //user is not logged in
         console.log("no user id");
-        res.redirect('/user/login');
+        res.locals.status = 406;
+        return next('route');
     }
 }
