840f63edbeaa34e5f6a52e4a7ecfa8332c5df1dc
Andy Situ <and.situ@gmail.com>
2020-05-31 20:07:49 -0700

    move application_helper to application_form.js

--- public/javascripts/care/application_form.js
+++ public/javascripts/care/application_form.js
@@ -1,5 +1,31 @@
+// Check the form, in case html required doesn't work
+var application_helper = {
+  check_waiver() {
+    return ($('input[name=waiver-radio]:checked').val() == "1");
+  },
+  check_inputs() {
+    var filled_out = true;
+
+    function check_element(index, element) {
+      if ($(this).val().length <= 0) {
+        $(this).css("border-color", "red");
+        filled_out = false;
+      }
+    }
+    
+    $('input[notrequired]').each(check_element);
+    $('textarea[notrequired]').each(check_element);
+    $('radio[notrequired]').each(check_element);
+    return filled_out;
+  },
+  check_marital() {
+    return ($('input[name=marital_status]:checked').val() !== undefined);
+  },
+};
+
 window.onload = function() {
   $("#applicant-form").on("submit", function(e) {
+    // Check that some inputs are filled out and send data
     e.preventDefault();
 
     if (!application_helper.check_inputs()) {
