46563083676ea5db8a4495c80aa8200119cff806
Andy Situ <and.situ@gmail.com>
2020-06-02 15:54:27 -0700

    update service page printing
    Due to Chrome bug that prevents

--- public/javascripts/care/service_page.js
+++ public/javascripts/care/service_page.js
@@ -14,8 +14,8 @@ var services_handler = {
   add_print_btn_handler() {
     $("#print-btn").on("click", this.print_btn_handler);
   },
-  print_btn_handler(e) {
-    // Remove scroll bar from notes for printing
+  modify_page_for_print() {
+    // Remove scroll bar from notes for printing & padding in card
     var $note_container = $("#notes-container");
     $note_container.addClass("print");
     $note_container.removeClass("scrollbox");
@@ -26,19 +26,44 @@ var services_handler = {
 
     $(".card-body").each(function(index, element) {
       $(element).addClass("printbox");
-    })
-
-    window.print();
-
+    });
+  },
+  restore_page_after_print() {
+    // Place back scroll bars & padding in card
     $note_container.removeClass("print");
     $note_container.addClass("scrollbox");
 
     $(".card-header").each(function(index, element) {
       $(element).removeClass("printbox");
-    })
+    });
     $(".card-body").each(function(index, element) {
       $(element).removeClass("printbox");
-    })
+    });
+  },
+  print_btn_handler(e) {
+    services_handler.modify_page_for_print();
+
+    // Make 3 columns into 1 for printing
+    var div1 = $("#first-container"),
+        div2 = $("#second-container"),
+        div3 = $("third-container");
+
+    var div1_class = div1.attr("class"),
+        div2_class = div2.attr("class"),
+        div3_class = div3.attr("class");
+
+    div1.removeClass();
+    div2.removeClass();
+    div3.removeClass();
+
+    window.print();
+
+    // restore back to 3 columns
+    div1.attr("class", div1_class);
+    div2.attr("class", div2_class);
+    div3.attr("class", div3_class);
+    
+    services_handler.restore_page_after_print();
   },
   add_note(note, date) {
     var div = $('<div class="noteDiv"></div>');
