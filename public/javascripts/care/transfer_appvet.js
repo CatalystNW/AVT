cbf52dbbfb02a376dc2b982c7f95ae76ac4ee43c
Andy Situ <and.situ@gmail.com>
2020-06-02 12:24:19 -0700

    set query parameters to search form after results load

--- public/javascripts/care/transfer_appvet.js
+++ public/javascripts/care/transfer_appvet.js
@@ -13,4 +13,19 @@ window.onload = function() {
       }
     });
   });
+
+  this.set_query_parameters();
 };
+
+// Checks if there are query strings. If so, set the search parameters to that.
+function set_query_parameters() {
+  var url_parameters = new URLSearchParams(window.location.search);
+
+  if (url_parameters.has('search_option')) {
+    $("#yearSelect").val(url_parameters.get('search_option'));
+  }
+
+  if (url_parameters.has('search_value')) {
+    $("#search_input").val(url_parameters.get('search_value'));
+  }
+}
