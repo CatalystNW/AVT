c1b2c95c6f683dc75f5b66e97c073484b99d38c8
Rohin Adalja <rohin.adalja@mail.utoronto.ca>
2020-07-26 02:36:11 -0400

    add AVT_CREATE_NEW_USER to .env

--- script/createServiceUsers.sh
+++ script/createServiceUsers.sh
@@ -14,13 +14,13 @@ exit
 EOF
 
 case "$AVT_CREATE_NEW_USER" in
-    [yY][eE][sS]|[yY])
-        node ./script/createAdminUser &
-        sleep 3
+    [nN][oO]|[nN])
+        echo -e "AVT | SKIPPING CREATING AVT ADMIN USER: ENV VAR 'AVT_CREATE_NEW_USER' is set to 'no'"
+        echo -e "AVT | If you need to do this at a future time, you can run 'node /script/createAdminUser' manually."
         ;;
     *)
-          echo -e "AVT | SKIPPING CREATING AVT ADMIN USER: ENV VAR 'AVT_CREATE_NEW_USER' is not set to 'yes'"
-          echo -e "AVT | If you need to do this at a future time, you can run 'node /script/createAdminUser' manually."
+          node ./script/createAdminUser &
+          sleep 3
         ;;
 esac
 
