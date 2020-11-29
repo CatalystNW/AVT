e78cc226c441ca9dd2daf59fa0207b04fe7e829a
Andy Situ <and.situ@gmail.com>
2020-11-28 22:24:04 -0800

    Project: Assessetment menu, work item modal (frontend)

--- public/javascripts/app_project/site_assessment.js
+++ public/javascripts/app_project/site_assessment.js
@@ -1,3 +1,5 @@
+// Note: app_id is a variable set by the page
+
 var funkie = {
   load_application(app_id, callback) {
     $.ajax({
@@ -7,14 +9,132 @@ var funkie = {
         if (callback)
           callback(data);
       }
-    })
+    });
   },
+  calculate_page_height() {
+    return window.innerHeight - document.getElementById("cPart").offsetHeight
+    - document.getElementById("navbarResponsive").offsetHeight - 40
+    - document.getElementsByClassName("small")[0].offsetHeight;
+  },
+  get_assessment(app_id, callback) {
+    console.log("GET")
+    $.ajax({
+      type: "GET",
+      url: "../site_assessment/" + app_id,
+      success: function(data, textStatus, xhr) {
+        if (callback) {
+          callback(data);
+        }
+      }
+    });
+  },
+  create_workitem() {
+    $("#modalMenu").modal("hide");
+  }
+}
+
+class ModalMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "",
+      submit_form_callback: null,
+      title: ""
+    }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.submit_form_callback) {
+      this.state.submit_form_callback();
+    }
+  }
+
+  show_menu(type, submit_form_callback) {
+    if (type == "create_workitem") {
+      this.setState(
+        {type: type, title: "Create WorkItem", submit_form_callback: submit_form_callback});
+    }
+  }
+
+  create_menu() {
+    if (this.state.type == "") {
+      return <div></div>
+    } else if (this.state.type == "create_workitem") {
+      return (<div>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" className="form-control" name="name" id="name-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <textarea className="form-control" name="description" id="desc-input" required></textarea>
+        </div>
+        <div className="form-group">
+          <label>Assessment Comments</label>
+          <textarea className="form-control" name="description" id="comments-input"></textarea>
+        </div>
+        <div className="form-check">
+          <input type="checkbox" name="handleit" id="handleit-check"></input>
+          <label className="form-check-label" htmlFor="handleit-check">Handle-It</label>
+        </div>
+      </div>);
+    }
+  }
+
+  render() {
+    return (
+      <div className="modal" tabIndex="-1" role="dialog" id="modalMenu">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              {this.state.title}
+              <button type="button" className="close" data-dismiss="modal">
+                <span>&times;</span>
+              </button>
+            </div>
+            <form onSubmit={this.onSubmit}>
+              <div className="modal-body">
+                {this.create_menu()}
+              </div>
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-primary">Save</button>
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class AssessmentMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const divStyle = {
+      height: funkie.calculate_page_height().toString() + "px",
+    };
+
+    return (
+      <div className="col-sm-12 col-lg-6 overflow-auto" style={divStyle}
+        id="assessment-container" key={this.props.id}>
+          <h2>Work Items</h2>
+          <button type="button" className="btn btn-primary" onClick={this.props.set_workitem_menu}
+            data-toggle="modal" data-target="#modalMenu">
+            Create Work Item
+          </button>
+        </div>
+    )
+  }
 }
 
 class ApplicationInformation extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props.application);
   }
 
   calculate_age = () => {
@@ -33,10 +153,8 @@ class ApplicationInformation extends React.Component {
   render() {
     // set to browser height so that overflow will show both divs with scrollbars
     const divStyle = {
-      height: (window.innerHeight - document.getElementById("cPart").offsetHeight
-        - document.getElementById("navbarResponsive").offsetHeight - 40
-        - document.getElementsByClassName("small")[0].offsetHeight).toString() + "px",
-    }
+      height: funkie.calculate_page_height().toString() + "px",
+    };
     var app = this.props.application;
 
     const name = app.middle_name == "" ?
@@ -46,7 +164,10 @@ class ApplicationInformation extends React.Component {
       <span>{app.line_1}<br />{app.city}, {app.state} {app.zip}</span> :
       <span>{app.line_1}<br />{app.line_2}<br />{app.city}, {app.state} {app.zip}</span>
 
-    const owns_home = app.owns_home ? "Yes" : "No";
+    const owns_home = app.owns_home ? "Yes" : "No",
+          can_contribute = app.client_can_contribute ? app.client_can_contribute_description : "No",
+          ass_can_contribute = app.associates_can_contribute ? app.associates_can_contribute_description : "No";
+          
 
     var google_url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyD2CmgnSECdg_g-aFgp95NUBv2QUEidDvs&q=";
     google_url += `${app.line_1} ${app.line_2}, ${app.city}, ${app.state}, ${app.zip}`;
@@ -109,10 +230,12 @@ class ApplicationInformation extends React.Component {
                   <tr><th className="col-xs-3">Ownership Length</th><td className="col-xs-9">{app.ownership_length}</td></tr>
                   <tr><th className="col-xs-3">Years Constructed</th><td className="col-xs-9">{app.year_constructed}</td></tr>
                   <tr><th className="col-xs-3">Requested Repairs</th><td className="col-xs-9">{app.requested_repairs}</td></tr>
+                  <tr><th className="col-xs-3">Client Can Contribute</th><td className="col-xs-9">{can_contribute}</td></tr>
+                  <tr><th className="col-xs-3">Assoc. Can Contribute</th><td className="col-xs-9">{ass_can_contribute}</td></tr>
                 </tbody>
               </table>
             </div>
-            <div class="tab-pane" id="nav-map-info" role="tabpanel">
+            <div className="tab-pane" id="nav-map-info" role="tabpanel">
               <iframe width="100%" height="280" frameBorder="0"
                 src={google_url} target="_blank">Google Maps Link</iframe>
             </div>
@@ -127,9 +250,14 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      // both apps and assessments should be length 1, but use map to create them
       applications: [],
+      assessments: [],
     }
     this.getAppData();
+    this.getAssessment();
+    
+    this.modalmenu = React.createRef();
   }
 
   getAppData = () => {
@@ -139,15 +267,38 @@ class App extends React.Component {
     });
   }
 
+  getAssessment = () => {
+    var that = this;
+    funkie.get_assessment(app_id, function(data) {
+      console.log(data.site_assessment);
+      that.setState({assessments: [data.site_assessment,]});
+    });
+  }
+
+  set_workitem_menu =() => {
+    this.modalmenu.current.show_menu("create_workitem", funkie.create_workitem);
+  }
+
   render() {
     const application_information = this.state.applications.map((application) => {
       return <ApplicationInformation key={application.id} 
         application={application}></ApplicationInformation>
     });
+
+    const assessment_menu = this.state.assessments.map((assessment) => {
+      console.log(assessment);
+      return <AssessmentMenu 
+        key={assessment._id} id={assessment._id}
+        application_id={app_id} 
+        set_workitem_menu={this.set_workitem_menu}
+      />
+    });
     return (
       <div>
-        <div className="col-sm-12 col-lg-6 overflow-auto" id="application-input-container"></div>
+        {assessment_menu}
         {application_information}
+
+        <ModalMenu ref={this.modalmenu} />
       </div>);
   }
 }
