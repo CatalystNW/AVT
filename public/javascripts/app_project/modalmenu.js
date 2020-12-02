81596451e28ea79944cb90de2faf5c06ee86c3d9
Andy Situ <and.situ@gmail.com>
2020-12-01 19:45:34 -0800

    Project: move ModalMenu to separate js file

---
+++ public/javascripts/app_project/modalmenu.js
@@ -0,0 +1,168 @@
+class ModalMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "",
+      submit_form_callback: null,
+      title: "",
+      additional_data: null,
+      handle_data_callback: null,
+    }
+  }
+
+  close_menu() {
+    $("#modalMenu").modal("hide");
+    $("#modalmenu-form")[0].reset();
+  }
+
+  get_data = () => {
+    var data = {};
+    if (this.state.additional_data) {
+      for (var k in this.state.additional_data) {
+        data[k] = this.state.additional_data[k];
+      }
+    }
+
+    var formData = new FormData($("#modalmenu-form")[0]);
+
+    if (this.state.type=="create_workitem") {
+      formData.set("handleit", formData.get("handleit") == "on" ? true:false); 
+    }
+    for (var key of formData.keys()) {
+      data[key] = formData.get(key);
+    }
+    return data;
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.submit_form_handler) {
+      var data = this.get_data();
+      
+      this.state.submit_form_handler(
+        data, this.close_menu, this.state.handle_data_callback);
+    }
+  }
+
+  show_menu(type, submit_form_handler, additional_data, handle_data_callback) {
+    if (type == "create_workitem") {
+      this.setState(
+        { type: type, title: "Create WorkItem", 
+          submit_form_handler: submit_form_handler,
+          additional_data: additional_data,
+          handle_data_callback: handle_data_callback,
+      });
+    } else if (type == "create_materialsitem") {
+      this.setState(
+        {
+          type: type, title: "Create Item",
+          submit_form_handler: submit_form_handler,
+          additional_data: additional_data,
+          handle_data_callback: handle_data_callback,
+        });
+    } else if (type == "edit_materialsitem") {
+      this.setState(
+        {
+          type: type, title: "Edit Item",
+          submit_form_handler: submit_form_handler,
+          additional_data: {},
+          handle_data_callback: handle_data_callback,
+          prev_data: additional_data,
+        }
+      )
+    }
+  }
+
+  create_menu() {
+     if (this.state.type == "create_workitem") {
+      return (<div>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" className="form-control" name="name" id="name-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <textarea className="form-control" name="description" id="desc-input" required></textarea>
+        </div>
+        <div className="form-group">
+          <label>Assessment Comments</label>
+          <textarea className="form-control" name="assessment_comments" id="comments-input"></textarea>
+        </div>
+        <div className="form-check">
+          <input type="checkbox" name="handleit" id="handleit-check"></input>
+          <label className="form-check-label" htmlFor="handleit-check">Handle-It</label>
+        </div>
+      </div>);
+    } else if (this.state.type == "create_materialsitem") {
+      return (<div>
+        <div className="form-group">
+          <label>Description</label>
+          <input type="text" className="form-control" name="description" id="description-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Quantity</label>
+          <input type="number" className="form-control" name="quantity" id="quantity-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input type="number" className="form-control" step="any" name="price" id="price-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Vendor</label>
+          <input type="text" className="form-control" name="vendor" id="vendor-input" required></input>
+        </div>
+      </div>);
+    } else if (this.state.type == "edit_materialsitem") {
+      return (<div>
+        <div className="form-group">
+          <label>Description</label>
+          <input type="text" className="form-control" name="description" 
+            defaultValue={this.state.prev_data.description} id="description-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Quantity</label>
+          <input type="number" className="form-control" name="quantity" 
+            defaultValue={this.state.prev_data.quantity} id="quantity-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input type="number" className="form-control" step="any" name="price" 
+            defaultValue={this.state.prev_data.price} id="price-input" required></input>
+        </div>
+        <div className="form-group">
+          <label>Vendor</label>
+          <input type="text" className="form-control" name="vendor" 
+            defaultValue={this.state.prev_data.vendor} id="vendor-input" required></input>
+        </div>
+      </div>);
+    } else {
+      return <div></div>
+    }
+  }
+
+  render() {
+    return (
+      <div className="modal" tabIndex="-1" role="dialog" id="modalMenu">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              {this.state.title}
+              <button type="button" className="close" data-dismiss="modal">
+                <span>&times;</span>
+              </button>
+            </div>
+            <form onSubmit={this.onSubmit} id="modalmenu-form">
+              <div className="modal-body">
+                {this.create_menu()}
+              </div>
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-primary">Save</button>
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
