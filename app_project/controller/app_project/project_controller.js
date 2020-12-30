657392d8ae373e90b69df425f998f07b32bfec38
Andy Situ <and.situ@gmail.com>
2020-12-29 16:09:01 -0800

    Fill dummy projects_transfer page with app_id's of complete assessments

--- app_project/controller/app_project/project_controller.js
+++ app_project/controller/app_project/project_controller.js
@@ -7,5 +7,8 @@ var DocumentPackage = require("../../../models/documentPackage"),
 module.exports.view_project_assessment_transfer = view_project_assessment_transfer;
 
 async function view_project_assessment_transfer(req, res) {
-  res.render("app_project/project_transfer");
+  var assessments = await SiteAssessment.find({status: "complete",})
+                      .populate("documentPackage");
+  console.log(assessments);
+  res.render("app_project/project_transfer", {assessments: assessments,});
 }
