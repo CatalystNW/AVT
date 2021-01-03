80238edc5895bce9a6d3e5c17836346bbd10bd26
Andy Situ <and.situ@gmail.com>
2021-01-03 11:18:34 -0800

    Show workitems name in project transfer

--- public/javascripts/app_project/project_transfer.js
+++ public/javascripts/app_project/project_transfer.js
@@ -3,20 +3,60 @@ class ProjectTransferApp extends React.Component {
     super(props);
     this.state = {
       assessment_id: assessment_id,
+      handleit_workitems: [],
+      proj_workitems: []
     }
     this.load_assessment();
   }
   load_assessment = () => {
+    var that = this;
     $.ajax({
       url: "../site_assessment/" + this.state.assessment_id,
       method: "GET",
       success: function(data) {
-        console.log(data);
+        console.log(data)
+        var handleit_workitems = [],
+            proj_workitems = [], workitem;
+        for (var i=0; i< data.workItems.length; i++) {
+          workitem = data.workItems[i];
+          if (workitem.status == "accepted") {
+            if (workitem.handleit) {
+              handleit_workitems.push(workitem);
+            } else {
+              proj_workitems.push(workitem);
+            }
+          }
+        }
+        console.log(proj_workitems);
+        console.log(handleit_workitems);
+        
+        that.setState({
+          handleit_workitems: handleit_workitems,
+          proj_workitems: proj_workitems,
+        });
       },
     });
   }
+
+  create_workItems = (handleit) => {
+    var workitems = handleit ? 
+      this.state.handleit_workitems : this.state.proj_workitems;
+    return (<div>
+      {workitems.map((workitem) => {
+        return (
+          <div>
+            {workitem.name}
+          </div>
+        );
+      })}
+    </div>);
+  }
+
   render() {
-    return (<div></div>);
+    return (<div>
+      {this.create_workItems(true)}
+      {this.create_workItems(false)}
+    </div>);
   }
 }
 
