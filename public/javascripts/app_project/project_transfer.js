312b9ebff3d9acd2ccf227c427e943b0dca40bb8
Andy Situ <and.situ@gmail.com>
2021-01-02 21:24:11 -0800

    Update project_transfer.js

--- public/javascripts/app_project/project_transfer.js
+++ public/javascripts/app_project/project_transfer.js
@@ -1,6 +1,22 @@
 class ProjectTransferApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      assessment_id: assessment_id,
+    }
+    this.load_assessment();
+  }
+  load_assessment = () => {
+    $.ajax({
+      url: "../site_assessment/" + this.state.assessment_id,
+      method: "GET",
+      success: function(data) {
+        console.log(data);
+      },
+    });
+  }
   render() {
-    return (<div>{assessment_id}</div>);
+    return (<div></div>);
   }
 }
 
