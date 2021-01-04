efd51d6cf31ae5236918177574e158e1d8c892c1
Andy Situ <and.situ@gmail.com>
2021-01-04 11:53:05 -0800

    Update project_transfer_controller.js

--- app_project/controller/app_project/project_transfer_controller.js
+++ app_project/controller/app_project/project_transfer_controller.js
@@ -1,19 +1,18 @@
 var DocumentPackage = require("../../../models/documentPackage"),
     SiteAssessment = require("../../models/app_project/SiteAssessment");
 
-    module.exports.view_project_transfers = view_project_transfers;
-    module.exports.view_project_transfer = view_project_transfer;
-    
-    async function view_project_transfers(req, res) {
-      var assessments = await SiteAssessment.find({status: "complete",})
-                          .populate("documentPackage");
-      console.log(assessments);
-      res.render("app_project/project_transfers", {assessments: assessments,});
-    }
-    
-    async function view_project_transfer(req, res) {
-      // var assessment = await SiteAssessment.findById(req.params.assessment_id)
-      //     .populate("documentPackage")
-      //     .populate({path: "workItems", model: "WorkItem", populate: {path: "materialsItems", model: "MaterialsItem"}});
-      res.render("app_project/project_transfer", {assessment_id: req.params.assessment_id,});
-    }
+module.exports.view_project_transfers = view_project_transfers;
+module.exports.view_project_transfer = view_project_transfer;
+
+async function view_project_transfers(req, res) {
+  var assessments = await SiteAssessment.find({status: "complete",})
+                      .populate("documentPackage");
+  res.render("app_project/project_transfers", {assessments: assessments,});
+}
+
+async function view_project_transfer(req, res) {
+  // var assessment = await SiteAssessment.findById(req.params.assessment_id)
+  //     .populate("documentPackage")
+  //     .populate({path: "workItems", model: "WorkItem", populate: {path: "materialsItems", model: "MaterialsItem"}});
+  res.render("app_project/project_transfer", {assessment_id: req.params.assessment_id,});
+}
