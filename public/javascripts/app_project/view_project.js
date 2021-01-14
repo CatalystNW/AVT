190227366b0ff23d8f9dfe6ad5207189fd2aed25
Andy Situ <and.situ@gmail.com>
2021-01-14 13:00:37 -0800

    Create dummy ProjectMenu jsx

--- public/javascripts/app_project/view_project.js
+++ public/javascripts/app_project/view_project.js
@@ -36,10 +36,66 @@ class AppProject extends React.Component {
   render() {
     return (
     <div>
+      <ProjectMenu />
       <ApplicationInformation
         application={this.state.application} 
       />
     </div>);
+  }
+}
+
+class ProjectMenu extends React.Component {
+  render () {
+    const divStyle = {
+      height: funkie.calculate_page_height().toString() + "px",
+    };
+    return (<div className="col-sm-12 col-lg-8" style={divStyle}
+        id="assessment-container">
+        <div id="assessment-nav-container">
+          <ul className="nav nav-tabs" id="nav-assessment-tabs" role="tablist">
+            <li className="nav-item">
+              <a className="nav-link active" id="nav-property-tab" data-toggle="tab" 
+                href="#nav-workitem" role="tab">Work Items</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" id="nav-cost-summary-tab" data-toggle="tab" 
+                href="#nav-cost-summary" role="tab">Cost Summary</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" id="nav-checklist-tab" data-toggle="tab" 
+                href="#nav-checklist" role="tab">Checklist</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="tab-content overflow-auto" id="nav-assessment-tabContent">
+          {/* <div className="tab-pane" id="nav-checklist" role="tabpanel">
+            <AssessmentChecklist ref={this.checklist}
+              assessment={{}}
+              vetting_summary = {this.props.vetting_summary}
+            />
+          </div>
+          <div className="tab-pane" id="nav-cost-summary" role="tabpanel">
+            <CostSummary ref={this.costsummary}/>
+          </div>
+          <div className="tab-pane show active" id="nav-workitem" role="tabpanel">
+            <button type="button" className="btn btn-primary" 
+              onClick={this.props.set_create_workitem_menu}>
+              Create Work Item
+            </button>
+            {this.state.workItems.map((workitem, index) => {
+              return (
+              <WorkItem
+                workitem={workitem}
+                remove_workitem={this.remove_workitem}
+                set_edit_materialisitem_menu={this.props.set_edit_materialisitem_menu}
+                set_create_materialsitem_menu={this.props.set_create_materialsitem_menu}
+                set_edit_workitem_menu = {this.props.set_edit_workitem_menu}
+                key={workitem._id+"-workitem-card"}></WorkItem>);
+            })}
+          </div> */}
+        </div>
+      </div>);
   }
 }
 
