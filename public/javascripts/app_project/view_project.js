16017a62f7b158f34c8edd19e9245436ef92148c
Andy Situ <and.situ@gmail.com>
2021-01-14 12:13:07 -0800

    Update view_project.js

--- public/javascripts/app_project/view_project.js
+++ public/javascripts/app_project/view_project.js
@@ -2,7 +2,20 @@ class AppProject extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      projects: [],
+      project: {},
+    };
+    this.load_project();
+  }
+
+  load_project() {
+    if (project_id) {
+      $.ajax({
+        url: "/app_project/projects/" + project_id,
+        type: "GET",
+        success: function(data) {
+          console.log(data);
+        }
+      })
     }
   }
 
