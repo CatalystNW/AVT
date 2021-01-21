e8a20a03dae9a4511095e9d651a67a1ee52ff294
Andy Situ <and.situ@gmail.com>
2021-01-21 12:01:59 -0800

    Correct checking whether assessment exists in get requests

--- app_project/controller/app_project/assessment_controller.js
+++ app_project/controller/app_project/assessment_controller.js
@@ -76,7 +76,7 @@ async function get_site_assessment_by_appId(req, res) {
     var site_assessment = await SiteAssessment.find({application_id: app_id})
         .populate({path:"workItems", model: "WorkItem", populate: {path:"materialsItems", model: "MaterialsItem"}})
         .populate("documentPackage").exec();
-    if (!site_assessment || site_assessment.length == 0) {
+    if (site_assessment.length == 0) {
       // The other fields won't exist at creation
       site_assessment = await SiteAssessment.create(app_id);
     } else {
