096f24545187e6e8a477195e85956a8ee45bf923
Andy Situ <and.situ@gmail.com>
2021-01-24 15:03:11 -0800

    Set type (planning & wraput0 for planningchecklist

--- public/javascripts/app_project/project/planningchecklist.js
+++ public/javascripts/app_project/project/planningchecklist.js
@@ -9,8 +9,14 @@ class PlanningChecklist extends React.Component {
   }
 
   load_checklist = () => {
+    var url;
+    if (this.props.type == "planning") {
+      url = "../projects/" + this.props.project_id + "/plan_checklist";
+    } else {
+      url = "../projects/" + this.props.project_id + "/wrapup_checklist";
+    }
     $.ajax({
-      url: "../projects/" + this.props.project_id + "/plan_checklist",
+      url: url,
       type: "GET",
       context: this,
       success: function(checklist_data) {
@@ -25,29 +31,56 @@ class PlanningChecklist extends React.Component {
   // Maps table rows with the backend data properties
   // Note: uses ES6+ with some order in objects
   get_table_map = () => {
-    return {
-      contract_mailed_to_client: "Contract mailed to Client",
-      create_endis: "Create project webpage, calendar event, and volunteer schedule in Endis",
-      email_req_volunteers_and_register: "Send out initial email requesting volunteers. Register into Website/DB",
-      proj_activation_call_client: "Project activation call to client",
-      planning_visit: "Planning visit completed",
-      create_cost_lists: "Create list of materials, rentals & supplies needed. Send to Darrell",
-      verify_site_resources: "Verify site resources needed",
-      arranged_purchase_delivery: "Arrange for purchase & delivery of all materials, rentals, supplies, etc.",
-      rent_pota_potty: "Rent porta-pottie",
-      rent_waste_bin: "Rent waste bin",
-      check_weather: "Check the weather forecast and make plans accordingly",
-      verify_number_volunteers: "Verify number of volunteers signed up",
-      send_final_volunteer_email: "Send final email to signed up volunteers 3-5 days before project",
-      send_followup_volunteer_email: "Send follow-up emails to volunteers via Endis as needed",
+    if (this.props.type == "planning") {
+      return {
+        contract_mailed_to_client: "Contract mailed to Client",
+        create_endis: "Create project webpage, calendar event, and volunteer schedule in Endis",
+        email_req_volunteers_and_register: "Send out initial email requesting volunteers. Register into Website/DB",
+        proj_activation_call_client: "Project activation call to client",
+        planning_visit: "Planning visit completed",
+        create_cost_lists: "Create list of materials, rentals & supplies needed. Send to Darrell",
+        verify_site_resources: "Verify site resources needed",
+        arranged_purchase_delivery: "Arrange for purchase & delivery of all materials, rentals, supplies, etc.",
+        rent_pota_potty: "Rent porta-pottie",
+        rent_waste_bin: "Rent waste bin",
+        check_weather: "Check the weather forecast and make plans accordingly",
+        verify_number_volunteers: "Verify number of volunteers signed up",
+        send_final_volunteer_email: "Send final email to signed up volunteers 3-5 days before project",
+        send_followup_volunteer_email: "Send follow-up emails to volunteers via Endis as needed",
+      }
+    } else {
+      return {
+        return_signup_sheet: "Return volunteer sign-up sheet to office",
+        record_volunteer_info: "Record volunteer hours & info",
+        schedule_porta_pickup: "Schedule porta potty pickup",
+        schedule_waste_pickup: "Schedule waste bin pickup",
+        arrange_waste_disposal: "Arrange waste disposal",
+        return_materials_rental: "Return materials & rentals",
+        turn_receipts_expenses: "Turn in receipts & expenses to office",
+        process_reimbursement_checks: "Process reimbursement checks",
+        submit_photos: "Submit photos to Flickr or office",
+        submit_project_form: "Submit project report to office",
+        update_project_webpage: "Update the project webpage",
+        call_client: "Call client to see how project went",
+        send_volunteer_email: "Send volunteer thank you emails",
+        determine_followup: "Determine if followup is needed",
+        sending_closing_letter: "Send closing recipient letter",
+      }
     }
+    
   }
 
   onChange_check_input = (e) => {
     var property = e.target.getAttribute("name"),
         value = e.target.checked;
+    var url;
+    if (this.props.type == "planning") {
+      url = "/app_project/plan_checklist/" + this.state.checklist._id;
+    } else {
+      url = "/app_project/wrapup_checklist/" + this.state.checklist._id;
+    }
     $.ajax({
-      url: "/app_project/plan_checklist/" + this.state.checklist._id,
+      url: url,
       type: "PATCH",
       data: {
         property: property,
@@ -79,8 +112,14 @@ class PlanningChecklist extends React.Component {
   onChange_owner_select = (e) => {
     var property = e.target.getAttribute("name"),
         value = e.target.value;
+    var url;
+    if (this.props.type == "planning") {
+      url = "/app_project/plan_checklist/" + this.state.checklist._id;
+    } else {
+      url = "/app_project/wrapup_checklist/" + this.state.checklist._id;
+    }
     $.ajax({
-      url: "/app_project/plan_checklist/" + this.state.checklist._id,
+      url: url,
       type: "PATCH",
       data: {
         property: property,
@@ -162,8 +201,14 @@ class PlanningChecklist extends React.Component {
           return;
         }
       }
+      var url;
+      if (this.props.type == "planning") {
+        url = "/app_project/plan_checklist/" + this.state.checklist._id;
+      } else {
+        url = "/app_project/wrapup_checklist/" + this.state.checklist._id;
+      }
       $.ajax({
-        url: "/app_project/plan_checklist/" + this.state.checklist._id,
+        url: url,
         type: "POST",
         data: {
           name: name,
@@ -190,8 +235,14 @@ class PlanningChecklist extends React.Component {
     if (!result) {
       return;
     }
+    var url;
+    if (this.props.type == "planning") {
+      url = "/app_project/plan_checklist/" + this.state.checklist._id;
+    } else {
+      url = "/app_project/wrapup_checklist/" + this.state.checklist._id;
+    }
     $.ajax({
-      url: "/app_project/plan_checklist/" + this.state.checklist._id,
+      url: url,
       type: "DELETE",
       data: {
         name: name,
