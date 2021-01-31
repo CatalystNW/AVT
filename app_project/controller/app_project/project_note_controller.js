19216388a6ea8ce762ec6d44b3976cfd4fa9d3cf
Andy Situ <and.situ@gmail.com>
2021-01-30 16:01:02 -0800

    Update project_note_controller.js

--- app_project/controller/app_project/project_note_controller.js
+++ app_project/controller/app_project/project_note_controller.js
@@ -1,8 +1,9 @@
 var AppProject      = require("../../models/app_project/AppProject"),
     AppProjectNote  = require("../../models/app_project/AppProjectNote");
 
-  module.exports = get_project_notes      = get_project_notes;
+  module.exports.get_project_notes      = get_project_notes;
 
   async function get_project_notes(req, res) {
+    console.log(req.params.project_id)
     res.status(200).send();
   }
