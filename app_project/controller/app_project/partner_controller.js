d422fbb17ba7ca7de4e6a35d13313fdd6befa293
Andy Situ <and.situ@gmail.com>
2021-02-04 11:43:51 -0800

    Update partner_controller  with create_partner

--- app_project/controller/app_project/partner_controller.js
+++ app_project/controller/app_project/partner_controller.js
@@ -2,9 +2,32 @@ var PartnerPackage  = require("../../../models/partnerPackage"),
     SiteAssessment  = require("../../models/app_project/SiteAssessment"),
     AppProject      = require("../../models/app_project/AppProject");
 
-module.exports.get_all_partners           = get_all_partners;
+module.exports.get_all_partners         = get_all_partners;
+module.exports.create_partner           =  create_partner
 
 async function get_all_partners(req, res) {
   var partners = await PartnerPackage.find({});
   res.status(200).json(partners);
+}
+
+async function create_partner(req, res) {
+  if (req.body.type == "project") {
+    var project = await AppProject.findById(req.body.project_id);
+
+    if (!project) {
+      res.status(404).end();
+      return;
+    }
+    var partner = new PartnerPackage();
+    partner.org_name      = req.body.name;
+    partner.org_address   = req.body.address;
+    partner.contact_name  = req.body.contact;
+    partner.contact_phone = req.body.phone;
+    partner.contact_email = req.body.email;
+    await partner.save();
+
+    project.partners.push(partner._id);
+    res.status(200).json(partner);
+  }
+  res.status(200).end();
 }
