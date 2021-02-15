93e9056457d102508deebd7b48f88ae345e1930f
Andy Situ <and.situ@gmail.com>
2021-02-15 13:11:08 -0800

    Create blank jsx page for projects transfer

---
+++ app_project/jsx_files/project_transfer/view_projects_transfer.jsx
@@ -0,0 +1,9 @@
+class ProjectsTransferApp extends React.Component {
+  render() {
+    return (<div></div>);
+  }
+}
+
+function loadReact() {
+  ReactDOM.render(<ProjectsTransferApp />, document.getElementById("transfer-container"));
+}
