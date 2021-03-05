1bf249754b524fb477a17c4c0d8c7a63c4d784e5
Andy Situ <and.situ@gmail.com>
2021-03-05 12:20:08 -0800

    Project Controller uses markComplete for AppProjects

--- app_project/controller/app_project/project_controller.js
+++ app_project/controller/app_project/project_controller.js
@@ -174,6 +174,10 @@ async function edit_project(req, res) {
     await project.save();
     res.status(200).send({"date": d});
   } else {
+    if (req.body.property == "status" &&
+        (req.body.value == "complete" || req.body.value == "withdrawn")) {
+      AppProject.markComplete(project_id);
+    }
     if (req.body.property && req.body.value) {
       project[req.body.property] = req.body.value;
       await project.save();
