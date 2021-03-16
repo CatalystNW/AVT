7adfdc11b99fd7eb14781ca7bfeb2a62c1064869
Andy Situ <and.situ@gmail.com>
2021-03-16 15:17:28 -0700

    Use AuthHelper in app_project route file

--- app_project/routes/app_project/app_project.js
+++ app_project/routes/app_project/app_project.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const authHelper = require("../../controller/app_project/AuthHelper");
+
 var document_controller       = require('../../controller/app_project/document_controller.js'),
     assessment_controller       = require('../../controller/app_project/assessment_controller.js'),
     project_controller          = require('../../controller/app_project/project_controller.js'),
@@ -14,59 +16,59 @@ var document_controller       = require('../../controller/app_project/document_c
 router.use('/site_assessments', require("./site_assessments.js"));
 
 router.route('/application/:application_id')
-  .get(assessment_controller.get_application_data_api);
+  .get(authHelper.checkLoggedInAPI, assessment_controller.get_application_data_api);
 
-router.patch('/document/:application_id/status', document_controller.editDocumentStatus);
-router.get('/application/:application_id/workitems', workitem_controller.getWorkitemsByAppId);
-router.post('/workitems', workitem_controller.create_workitem);
+router.patch('/document/:application_id/status', authHelper.checkLoggedInAPI, document_controller.editDocumentStatus);
+router.get('/application/:application_id/workitems', authHelper.checkLoggedInAPI, workitem_controller.getWorkitemsByAppId);
+router.post('/workitems', authHelper.checkLoggedInAPI, workitem_controller.create_workitem);
 router.route('/workitems/:workitem_id')
-  .patch(workitem_controller.edit_workitem)
-  .delete(workitem_controller.delete_workitem);
+  .patch(authHelper.checkLoggedInAPI, workitem_controller.edit_workitem)
+  .delete(authHelper.checkLoggedInAPI, workitem_controller.delete_workitem);
 
-router.post('/materialsitem', materialsitem_controller.create_materialsitem);
-router.delete('/materialsitem/:id', materialsitem_controller.delete_materialsitem);
-router.patch('/materialsitem/:id', materialsitem_controller.edit_materialsitem);
+router.post('/materialsitem', authHelper.checkLoggedInAPI, materialsitem_controller.create_materialsitem);
+router.delete('/materialsitem/:id', authHelper.checkLoggedInAPI, materialsitem_controller.delete_materialsitem);
+router.patch('/materialsitem/:id', authHelper.checkLoggedInAPI, materialsitem_controller.edit_materialsitem);
 
-router.get('/project_transfer/:assessment_id', project_transfer_controller.view_project_transfer);
-router.post('/project_transfer/:assessment_id', project_transfer_controller.transfer_project);
-router.get('/project_transfer', project_transfer_controller.view_project_transfers);
+router.get('/project_transfer/:assessment_id', authHelper.checkLoggedInPages, project_transfer_controller.view_project_transfer);
+router.post('/project_transfer/:assessment_id', authHelper.checkLoggedInAPI, project_transfer_controller.transfer_project);
+router.get('/project_transfer', authHelper.checkLoggedInPages, project_transfer_controller.view_project_transfers);
 
-router.get('/view_projects', project_controller.view_projects);
-router.get('/projects/assignable_users', project_controller.get_task_assignable_users);
-router.get('/projects', project_controller.get_projects);
-router.get('/projects/:project_id', project_controller.get_project);
-router.patch('/projects/:project_id', project_controller.edit_project);
+router.get('/view_projects', authHelper.checkLoggedInPages, project_controller.view_projects);
+router.get('/projects/assignable_users', authHelper.checkLoggedInAPI, project_controller.get_task_assignable_users);
+router.get('/projects', authHelper.checkLoggedInAPI, project_controller.get_projects);
+router.get('/projects/:project_id', authHelper.checkLoggedInAPI, project_controller.get_project);
+router.patch('/projects/:project_id', authHelper.checkLoggedInAPI, project_controller.edit_project);
 
-router.get('/view_projects/:project_id', project_controller.view_project);
+router.get('/view_projects/:project_id', authHelper.checkLoggedInPages, project_controller.view_project);
 
-router.get('/projects/:project_id/plan_checklist', project_controller.get_plan_checklist);
-router.get('/projects/:project_id/wrapup_checklist', project_controller.get_wrapup_checklist);
+router.get('/projects/:project_id/plan_checklist', authHelper.checkLoggedInAPI, project_controller.get_plan_checklist);
+router.get('/projects/:project_id/wrapup_checklist', authHelper.checkLoggedInAPI, project_controller.get_wrapup_checklist);
 
-router.post('/checklist/:checklist_id', project_controller.create_checklist_item);
-router.delete('/checklist/:checklist_id', project_controller.delete_checklist_item);
-router.patch('/checklist/:checklist_id', project_controller.edit_checklist);
+router.post('/checklist/:checklist_id', authHelper.checkLoggedInAPI,  project_controller.create_checklist_item);
+router.delete('/checklist/:checklist_id', authHelper.checkLoggedInAPI,  project_controller.delete_checklist_item);
+router.patch('/checklist/:checklist_id', authHelper.checkLoggedInAPI, project_controller.edit_checklist);
 
-router.get('/projects/:project_id/workitems', project_controller.get_work_items);
+router.get('/projects/:project_id/workitems', authHelper.checkLoggedInAPI, project_controller.get_work_items);
 
-router.get('/projects/:project_id/notes', project_note_controller.get_project_notes);
-router.post('/projects/:project_id/notes', project_note_controller.create_project_note);
-router.delete('/projects/:project_id/notes/:note_id', project_note_controller.delete_project_note);
-router.patch('/projects/:project_id/notes/:note_id', project_note_controller.edit_project_note);
+router.get('/projects/:project_id/notes', authHelper.checkLoggedInAPI, project_note_controller.get_project_notes);
+router.post('/projects/:project_id/notes', authHelper.checkLoggedInAPI, project_note_controller.create_project_note);
+router.delete('/projects/:project_id/notes/:note_id', authHelper.checkLoggedInAPI, project_note_controller.delete_project_note);
+router.patch('/projects/:project_id/notes/:note_id', authHelper.checkLoggedInAPI, project_note_controller.edit_project_note);
 
-router.patch('/projects/:project_id/partners', project_controller.set_partners);
+router.patch('/projects/:project_id/partners', authHelper.checkLoggedInAPI, project_controller.set_partners);
 
-router.delete('/partners/:partner_id', partner_controller.delete_partner);
-router.patch('/partners/:partner_id', partner_controller.edit_partner);
-router.get('/partners', partner_controller.get_all_partners);
-router.post('/partners', partner_controller.create_partner);
+router.delete('/partners/:partner_id', authHelper.checkLoggedInAPI, partner_controller.delete_partner);
+router.patch('/partners/:partner_id', authHelper.checkLoggedInAPI, partner_controller.edit_partner);
+router.get('/partners', authHelper.checkLoggedInAPI, partner_controller.get_all_partners);
+router.post('/partners', authHelper.checkLoggedInAPI, partner_controller.create_partner);
 
 
-router.get('/projects/paf_form/:project_id', project_controller.view_paf_page);
-router.get('/projects/handleit_form/:project_id', project_controller.view_handleit_form);
+router.get('/projects/paf_form/:project_id', authHelper.checkLoggedInPages, project_controller.view_paf_page);
+router.get('/projects/handleit_form/:project_id', authHelper.checkLoggedInPages, project_controller.view_handleit_form);
 
 // Temporarily delete projects & assessments for testing/development
-router.delete('/projects', development_controller.delete_all_projects);
-router.get('/delete_manager', development_controller.view_delete_manager);
-router.delete('/delete_manager', development_controller.manage_deletion);
+router.delete('/projects', authHelper.checkLoggedInAPI, development_controller.delete_all_projects);
+router.get('/delete_manager', authHelper.checkLoggedInPages, development_controller.view_delete_manager);
+router.delete('/delete_manager', authHelper.checkLoggedInAPI, development_controller.manage_deletion);
 
 module.exports = router;
