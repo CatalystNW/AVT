7d8cbe226ae84014008c2467cbacce0fa5a3ce80
Andy Situ <and.situ@gmail.com>
2021-03-18 19:27:23 -0700

    Use checkLoggedIn & checkIfCanView middleware for all pages at route level

--- app_project/routes/app_project/site_assessments.js
+++ app_project/routes/app_project/site_assessments.js
@@ -7,22 +7,22 @@ const authHelper = require("../../controller/app_project/AuthHelper");
 
 var assessment_controller = require('../../controller/app_project/assessment_controller.js');
 
-router.get('/view', authHelper.checkLoggedInPages, assessment_controller.view_site_assessments);
-router.get('/view/app_id/:application_id', authHelper.checkLoggedInPages, assessment_controller.view_site_assessment_by_app_id);
-router.get('/view/:assessment_id', assessment_controller.view_site_assessment);
+router.get('/view', authHelper.checkLoggedInPages, authHelper.checkIfCanView, assessment_controller.view_site_assessments);
+router.get('/view/app_id/:application_id', authHelper.checkLoggedInPages, authHelper.checkIfCanView, assessment_controller.view_site_assessment_by_app_id);
+router.get('/view/:assessment_id', authHelper.checkLoggedInPages, authHelper.checkIfCanView, assessment_controller.view_site_assessment);
 
-router.get('/transferred', authHelper.checkLoggedInAPI, assessment_controller.getTransferredAssessments);
-router.get('/to_transfer', authHelper.checkLoggedInAPI, assessment_controller.getToTransferAssessments);
-router.get('/applications', authHelper.checkLoggedInAPI, assessment_controller.getApplicationsInAssessment);
+router.get('/transferred', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.getTransferredAssessments);
+router.get('/to_transfer', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.getToTransferAssessments);
+router.get('/applications', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.getApplicationsInAssessment);
 
 router.route('/:assessment_id')
-  .get(authHelper.checkLoggedInAPI, assessment_controller.get_site_assessment)
-  .patch(authHelper.checkLoggedInAPI, assessment_controller.edit_site_assessment);
-router.patch('/:assessment_id/partners', authHelper.checkLoggedInAPI, assessment_controller.set_partners);
+  .get(authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.get_site_assessment)
+  .patch(authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.edit_site_assessment);
+router.patch('/:assessment_id/partners', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.set_partners);
 
-router.get('/paf_form/:assessment_id', authHelper.checkLoggedInAPI, assessment_controller.get_paf_page);
-router.get('/handleit_form/:assessment_id', authHelper.checkLoggedInAPI, assessment_controller.get_handleit_form);
+router.get('/paf_form/:assessment_id', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.get_paf_page);
+router.get('/handleit_form/:assessment_id', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.get_handleit_form);
 
-// router.get('/application/:application_id', authHelper.checkLoggedInAPI, assessment_controller.getSiteAssessmentByAppId);
+// router.get('/application/:application_id', authHelper.checkLoggedInAPI, authHelper.checkIfCanView, assessment_controller.getSiteAssessmentByAppId);
 
 module.exports = router;
