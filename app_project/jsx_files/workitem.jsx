254a4723f13669a373c96430be9207ad81bffdc6
Andy Situ <and.situ@gmail.com>
2021-03-19 16:20:44 -0700

    Prevent deleting workitem or materialsitem on vetting worksheet

--- app_project/jsx_files/workitem.jsx
+++ app_project/jsx_files/workitem.jsx
@@ -152,7 +152,9 @@ class WorkItem extends React.Component {
                           <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                         </svg>
                     </button>
-                    <button className="btn btn-outline-danger btn-sm" 
+                    { // Prevent deleting on vetting worksheet page
+                      this.props.page_type!="vetting" ? 
+                      (<button className="btn btn-outline-danger btn-sm" 
                       description={materialsItem.description}
                       item_id={materialsItem._id} 
                       onClick={this.onClick_delete_materialsitem}>
@@ -160,7 +162,8 @@ class WorkItem extends React.Component {
                           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                           <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
-                    </button>
+                    </button>)  : null
+                    }
                   </td>
                 </tr>)
             })}
@@ -241,7 +244,7 @@ class WorkItem extends React.Component {
               <button type="button" className="btn btn-sm btn-secondary"
                 onClick={this.onClick_edit_workitem_btn}>Edit</button></span>) : (null)
           }
-          {this.editable && !this.state.handleit ? 
+          {this.editable && !this.state.handleit && this.props.page_type!="vetting" ?
             (<span>
               <button type="button" className="btn btn-sm btn-danger"
                 onClick={this.onClick_del_workitem_btn}>Delete</button>
