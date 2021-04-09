6ed7485ed39e8fd49a6810e3883c52496b4ceaab
Andy Situ <and.situ@gmail.com>
2021-04-08 18:18:49 -0700

    Resizing project page will change heights of menu

--- app_project/jsx_files/project/view_project.jsx
+++ app_project/jsx_files/project/view_project.jsx
@@ -136,3 +136,12 @@ function loadReact() {
 }
 
 loadReact();
+
+window.onresize = function() {
+  const height = funkie.calculate_page_height().toString() + "px";
+  // Set Application Info Container
+  const appInfo = document.getElementById("application-info-container"),
+        project_menu = document.getElementById("assessment-container");
+  appInfo.style.height = height;
+  project_menu.style.height = height;
+}
