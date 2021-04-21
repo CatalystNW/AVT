60ca66b60dfd2a28ea13061a435a78ae4e886603
Andy Situ <and.situ@gmail.com>
2021-04-20 21:55:30 -0700

    Update report_controller with search_project

--- app_project/controller/app_project/report_controller.js
+++ app_project/controller/app_project/report_controller.js
@@ -7,6 +7,7 @@ const authHelper = require("./AuthHelper");
 
 module.exports.view_index_page = view_index_page;
 module.exports.get_upcoming_projects = get_upcoming_projects;
+module.exports.search_project = search_project;
 
 async function view_index_page(req, res) {
   res.render("app_project/report", {});
@@ -18,5 +19,22 @@ async function get_upcoming_projects(req, res) {
                     populate: {path:"materialsItems", model: "MaterialsItem"}})
                   .populate("partners")
                   .populate("documentPackage");
+  res.status(200).json(projects);
+}
+
+async function search_project(req, res) {
+  let options = {status: "complete"};
+  if (req.body.startDate || req.body.endDate) {
+    options.start = {};
+    if (req.body.startDate) {
+      options.start["$gte"] = new Date(req.body.startDate);
+    }
+    if (req.body.endDate) {
+      let endDate = new Date(req.body.endDate);
+      endDate.setHours(23, 59, 59, 59);
+      options.start["$lte"] = endDate;
+    }
+  }
+  let projects = await AppProject.find(options);
   res.status(200).json(projects);
 }
