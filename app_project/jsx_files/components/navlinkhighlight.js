94bdd11430a3d7a88769a8cd4de4b19cf85dca03
Andy Situ <and.situ@gmail.com>
2021-05-23 19:09:14 -0700

    Set active nav links depending on current url via js

---
+++ app_project/jsx_files/components/navlinkhighlight.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+  const pathname = window.location.pathname;
+  
+  let navId;
+  if (pathname.includes("carenetwork")) {
+    navId = "care-nav-link"
+  } else if (pathname.includes("/app_project/site_assessments/")) {
+    navId = "assessments-nav-link";
+  } else if (pathname.includes("/app_project/view_projects")) {
+    navId = "projects-nav-link";
+  } else {
+    switch(pathname) {
+      case "/view":
+        navId = "vetting-nav-link";
+        break;
+      case "/tasks":
+        navId = "tasks-nav-link";
+        break;
+      case "/user/userList":
+        navId = "userse-nav-link";
+        break;
+      case "/app_project/view_projects":
+        navId = "projects-nav-link";
+        break;
+      case "/app_project/report/view":
+        navId = "report-nav-link";
+        break;
+      case "/site":
+      case "/projectsummary":
+      case "/projectreport":
+        navId = "previous-nav-link";
+        break;
+    }
+  }
+  if (navId) {
+    const link = document.getElementById(navId);
+    console.log(link);
+    if (link) {
+      link.classList.add("active");
+    }
+  }
+  
+}
